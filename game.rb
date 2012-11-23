2a6ffb521cba4d87f6b0e347e79443c7f7bf5ed0
Daniel Berg <daniel.berg@it-gymnasiet.se>
2012-11-23 10:23:51 +0100

    Added Player class

--- game.rb
+++ game.rb
@@ -6,7 +6,18 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
 	end
+end
+
+class Player < Chingu::GameObject
+
+	#meta-constructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["ship.png"]
+	end
+
 end
 
 Game.new.show
